a13ddd26590031e8f11419e47f6aa883dbd40184
Justin Yu <justiny@vmware.com>
2014-02-08 02:48:54 -0800

    Add the lifecyccle spec for cloud.
    Change-Id: Ie8b15c0634d984838810a6ace567ca29eee2ebf1

---
+++ spec/integration/lifecycle_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+
+describe VCloudCloud::Cloud do
+  before(:all) do
+    @host          = ENV['BOSH_VCLOUD_CPI_URL']     || raise("Missing BOSH_VCLOUD_CPI_URL")
+    @user          = ENV['BOSH_VCLOUD_CPI_USER']     || raise("Missing BOSH_VCLOUD_CPI_USER")
+    @password      = ENV['BOSH_VCLOUD_CPI_PASSWORD'] || raise("Missing BOSH_VCLOUD_CPI_PASSWORD")
+    @vlan          = ENV['BOSH_VCLOUD_CPI_NET_ID']         || raise("Missing BOSH_VCLOUD_CPI_NET_ID")
+    @stemcell_path = ENV['BOSH_VCLOUD_CPI_STEMCELL']     || raise("Missing BOSH_VCLOUD_STEMCELL")
+    @org           = ENV['BOSH_VCLOUD_CPI_ORG']     || raise("Missing BOSH_VCLOUD_CPI_ORG")
+    @vdc           = ENV['BOSH_VCLOUD_CPI_VDC']     || raise("Missing BOSH_VCLOUD_CPI_VDC")
+    @vapp_catalog  = ENV['BOSH_VCLOUD_CPI_VAPP_CATALOG'] || raise("Missing BOSH_VCLOUD_CPI_VAPP_CATALOG")
+    @media_catalog = ENV['BOSH_VCLOUD_CPI_MEDIA_CATALOG']         || raise("Missing BOSH_VCLOUD_CPI_MEDIA_CATALOG")
+    @storage_prof  = ENV['BOSH_VCLOUD_CPI_MEDIA_STORAGE_PROFILE']     || raise("Missing BOSH_VCLOUD_CPI_MEDIA_STORAGE_PROFILE")
+    @metadata_key  = ENV['BOSH_VCLOUD_CPI_VM_METADATA_KEY']     || raise("Missing BOSH_VCLOUD_CPI_VM_METADATA_KEY")
+    @target_ip     = ENV['BOSH_VCLOUD_CPI_IP']     || raise("Missing BOSH_VCLOUD_CPI_IP")
+    @netmask       = ENV['BOSH_VCLOUD_CPI_NETMASK'] || raise("Missing BOSH_VCLOUD_CPI_NETMASK")
+    @dns           = ENV['BOSH_VCLOUD_CPI_DNS']         || raise("Missing BOSH_VCLOUD_CPI_DNS")
+    @gateway       = ENV['BOSH_VCLOUD_CPI_GATEWAY']     || raise("Missing BOSH_VCLOUD_CPI_GATEWAY")
+  end
+
+  before(:all) do
+    @cpi = described_class.new(
+      'agent' => {
+        'ntp' => ENV['BOSH_VCLOUD_CPI_NTP_SERVER'],
+      },
+      'vcds' => [{
+        'url' => @host,
+        'user' => @user,
+        'password' => @password,
+        'entities' => {
+          'organization' => @org,
+          'virtual_datacenter' => @vdc,
+          'vapp_catalog' => @vapp_catalog,
+          'media_catalog' => @media_catalog,
+          'media_storage_profile' => @storage_prof,
+          'vm_metadata_key' => @metadata_key,
+          'description' => 'MicroBosh on vCloudDirector',
+        }
+      }]
+    )
+
+  end
+
+  let(:cpi) { @cpi }
+
+  before(:all) do
+    Dir.mktmpdir do |temp_dir|
+      output = `tar -C #{temp_dir} -xzf #{@stemcell_path} 2>&1`
+      raise "Corrupt image, tar exit status: #{$?.exitstatus} output: #{output}" if $?.exitstatus != 0
+      @stemcell_id = @cpi.create_stemcell("#{temp_dir}/image", nil)
+    end
+  end
+
+  after(:all) { cpi.delete_stemcell(@stemcell_id) if @stemcell_id }
+
+  before { @vm_id = nil }
+  after { cpi.delete_vm(@vm_id) if @vm_id }
+
+  before { @disk_id = nil }
+  after { cpi.delete_disk(@disk_id) if @disk_id }
+
+  def vm_lifecycle(network_spec, disk_locality)
+    resource_pool = {
+      'ram' => 1024,
+      'disk' => 2048,
+      'cpu' => 1,
+    }
+
+    @vm_id = cpi.create_vm(
+      'agent-007',
+      @stemcell_id,
+      resource_pool,
+      network_spec,
+      disk_locality,
+      {'key' => 'value'}
+    )
+
+    @vm_id.should_not be_nil
+    cpi.has_vm?(@vm_id).should be(true)
+
+    @disk_id = cpi.create_disk(2048, @vm_id)
+    @disk_id.should_not be_nil
+
+    cpi.attach_disk(@vm_id, @disk_id)
+
+    cpi.detach_disk(@vm_id, @disk_id)
+  end
+
+  describe 'vcloud' do
+    let(:network_spec) do
+      {
+        "static" => {
+          "ip" => @target_ip,
+          "netmask" => @netmask,
+          "cloud_properties" => {"name" => @vlan},
+          "default" => ["dns", "gateway"],
+          "dns" => @dns,
+          "gateway" => @gateway
+        }
+      }
+    end
+
+    context 'without existing disks' do
+      it 'should exercise the vm lifecycle' do
+        vm_lifecycle(network_spec, [])
+      end
+    end
+
+    context 'with existing disks' do
+      before { @existing_volume_id = cpi.create_disk(2048) }
+      after { cpi.delete_disk(@existing_volume_id) if @existing_volume_id }
+
+      it 'should exercise the vm lifecycle' do
+        vm_lifecycle(network_spec, [@existing_volume_id])
+      end
+    end
+  end
+end
