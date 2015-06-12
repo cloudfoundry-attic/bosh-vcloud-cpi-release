2151601d31f112b83091f8ac2e42ed06d1863919
Jonathan Barnes <jbarnes@pivotal.io>
2015-06-12 15:34:06 -0700

    Saves the state of the ubuntu-manual director deployment
    [#73636458](https://www.pivotaltracker.com/story/show/73636458)
    
    Signed-off-by: Liang Biao <rexxar.liang@huawei.com>

---
+++ ci/tasks/save-deployment.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+source bosh-cpi-release/ci/tasks/utils.sh
+
+check_param base_os
+check_param network_type_to_test
+
+cpi_release_name=bosh-vcloud-cpi
+
+manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
+
+echo "checking in BOSH deployment state"
+cd deploy/$manifest_dir
+git add $base_os-$network_type_to_test-director-manifest-state.json
+git config --global user.email "cf-bosh-eng+bosh-ci@pivotal.io"
+git config --global user.name "bosh-ci"
+git commit -m ":airplane: Concourse auto-updating deployment state for bats pipeline, on $base_os/$network_type_to_test"
