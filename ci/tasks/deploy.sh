88470e701c81b20f7b9ec17e27d64133d42994f5
Aaron Hurley <ahurley@pivotal.io>
2015-06-17 17:15:21 -0700

    Update task script to use paths relative to the manifest, not the working directory.
    This is due to recent bosh-init changes.
    
    [#73636458](https://www.pivotaltracker.com/story/show/73636458)
    
    Signed-off-by: Liang Biao <rexxar.liang@huawei.com>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -14,17 +14,17 @@ print_git_state bosh-cpi-release
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
 semver=`cat version-semver/number`
-manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
-manifest_filename=$manifest_dir/$base_os-$network_type_to_test-director-manifest.yml
+manifest_dir=bosh-concourse-ci/pipelines/${cpi_release_name}
+manifest_filename=${manifest_dir}/${base_os}-${network_type_to_test}-director-manifest.yml
 
 echo "normalizing paths to match values referenced in $manifest_filename"
-mkdir ./tmp
-mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
-mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
-mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
+mkdir "${manifest_dir}/tmp"
+mv "./bosh-cpi-dev-artifacts/${cpi_release_name}-${semver}.tgz" "${manifest_dir}/tmp/${cpi_release_name}.tgz"
+mv "./bosh-release/release.tgz" "${manifest_dir}/tmp/bosh-release.tgz"
+mv "./stemcell/stemcell.tgz" "${manifest_dir}/tmp/stemcell.tgz"
 
 initver=$(cat bosh-init/version)
-initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
+initexe="${PWD}/bosh-init/bosh-init-${initver}-linux-amd64"
 chmod +x $initexe
 
 echo "deleting existing BOSH Director VM..."
