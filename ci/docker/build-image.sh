ee3b84a0e7392e5267db7458c6e4feabe8f711a6
Lyle Franklin <lfranklin@pivotal.io>
2016-03-02 09:00:28 -0800

    Allow build-image to be run from any working dir
    [#111543866](https://www.pivotaltracker.com/story/show/111543866)

--- ci/docker/build-image.sh
+++ ci/docker/build-image.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+docker_dir="$( cd $(dirname $0) && pwd )"
+
 DOCKER_IMAGE=${DOCKER_IMAGE:-boshcpi/vcloud-cpi-release}
 
 docker login
 
 echo "Building docker image..."
-docker build -t $DOCKER_IMAGE .
+docker build -t $DOCKER_IMAGE ${docker_dir}
 
 echo "Pushing docker image to '$DOCKER_IMAGE'..."
 docker push $DOCKER_IMAGE
