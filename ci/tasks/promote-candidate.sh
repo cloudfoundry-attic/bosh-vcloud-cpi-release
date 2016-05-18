2c9acb7aa49d4e75620ac425f1274d35cf812866
Felix Riegger <felix.riegger@sap.com>
2016-05-18 10:22:34 -0700

    Use outputs for promote job
    - the promote job was not yet migrated to explicit output usage, which
      is required by concourse
    
    [#119840805](https://www.pivotaltracker.com/story/show/119840805)
    
    Signed-off-by: Kam Leung <kleung@pivotal.io>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -x
 
-source bosh-cpi-release/ci/tasks/utils.sh
+: ${AWS_ACCESS_KEY_ID:?}
+: ${AWS_SECRET_ACCESS_KEY:?}
 
-check_param aws_access_key_id
-check_param aws_secret_access_key
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
 
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
 integer_version=`cut -d "." -f1 release-version-semver/number`
-echo $integer_version > integer_version
+echo $integer_version > promoted/integer_version
 
-cd bosh-cpi-release
-source /etc/profile.d/chruby.sh
-chruby 2.1.2
+cp -r bosh-cpi-src promoted/repo
 
-echo creating config/private.yml with blobstore secrets
-cat > config/private.yml << EOF
+dev_release=$(echo $PWD/bosh-cpi-release/*.tgz)
+
+pushd promoted/repo
+  set +x
+  echo creating config/private.yml with blobstore secrets
+  cat > config/private.yml << EOF
 ---
 blobstore:
   s3:
-    access_key_id: $aws_access_key_id
-    secret_access_key: $aws_secret_access_key
+    access_key_id: $AWS_ACCESS_KEY_ID
+    secret_access_key: $AWS_SECRET_ACCESS_KEY
 EOF
 
-echo "using bosh CLI version..."
-bosh version
+  echo "using bosh CLI version..."
+  bosh version
 
-echo "finalizing CPI release..."
-bosh finalize release ../bosh-cpi-dev-artifacts/*.tgz --version $integer_version
+  echo "finalizing CPI release..."
+  bosh finalize release ${dev_release} --version $integer_version
 
-rm config/private.yml
+  rm config/private.yml
 
-git diff | cat
-git add .
+  git diff | cat
+  git add .
 
-git config --global user.email cf-bosh-eng@pivotal.io
-git config --global user.name CI
-git commit -m ":airplane: New final release v $integer_version"
+  git config --global user.email cf-bosh-eng@pivotal.io
+  git config --global user.name CI
+  git commit -m ":airplane: New final release v $integer_version"
+popd
